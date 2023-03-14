dfbf739265a9a21c6590937632dbba0a60c2d6c0
Daniel Martinez <d.martinez74@gmail.com>
2023-03-14 01:31:23 +0000

    Domain Generator

--- src/app.js
+++ src/app.js
@@ -8,4 +8,19 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
+
+  var pronoun = ["the", "our", "my", "your", "his", "her", "its"];
+  var adj = ["great", "big", "black", "easy", "full", "human", "small"];
+  var noun = ["jogger", "racoon", "president", "dog", "shoe", "coder", "fish"];
+  var ext = [".com", ".co", ".edu", ".org", ".gov", ".io", ".biz"];
+
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        for (let l = 0; l < ext.length; l++) {
+          console.log(`${pronoun[i]}${adj[j]}${noun[k]}${ext[l]}`);
+        }
+      }
+    }
+  }
 };
